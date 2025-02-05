1b1bfe8d035b6a347799beb0b18e04bf6e255a32
protoss70 <tlegendgaming@gmail.com>
2025-02-05 12:15:27 +0100

    apiKey hidden

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,23 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAaPCPPFY8_zi4lJDd9BtCju_XL_C8c-cY",
-  authDomain: "quizabble.firebaseapp.com",
-  projectId: "quizabble",
-  storageBucket: "quizabble.firebasestorage.app",
-  messagingSenderId: "508178819548",
-  appId: "1:508178819548:web:89862b04367dda1a580719",
-  measurementId: "G-JXDMJCQBYD"
-};
+// Decode the Base64 environment variable and parse it into JSON
+const firebaseConfigBase64 = process.env.FIREBASE_CONFIG_BASE64;
+
+if (!firebaseConfigBase64) {
+  throw new Error("FIREBASE_CONFIG_BASE64 environment variable is missing.");
+}
+
+const firebaseConfigJson = Buffer.from(firebaseConfigBase64, "base64").toString("utf-8");
+const firebaseConfig = JSON.parse(firebaseConfigJson);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export { app, analytics }
+export { app, analytics };
