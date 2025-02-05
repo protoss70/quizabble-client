579aebe822b4f3701652c0020dd2f87fc331e03c
protoss70 <tlegendgaming@gmail.com>
2025-02-05 12:37:17 +0100

    tailwind setup

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage: React.FC = () => {
     return (
         <div>
-            <h1>Welcome to Quizabble</h1>
+            <h1 className=' text-lg text-orange-400 font-black'>Welcome to Quizabble</h1>
             <p>This is the home page.</p>
         </div>
     );
